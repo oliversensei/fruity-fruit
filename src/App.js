8edabaa44d9fd07d1ad68e6e6b1aee422470b5ca
John Oliver <jnomartillos@kld.edu.ph>
2024-11-13 03:16:36 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
-import PlacceOrder from './pages/PlaceOrder/PlaceOrder'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 
@@ -12,15 +12,19 @@ const App = () => {
 
   const [showLogin, setShowLogin] = useState(false);
 
+  useEffect(() => {
+    window.location.href = 'https://fruity-fruit.vercel.app/';
+  }, []);
+
   return (
     <>
-      {showLogin? <LoginPopup setShowLogin={setShowLogin} /> :<></>}
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : null}
       <main className='app'>
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
-          <Route path='/order' element={<PlacceOrder />} />
+          <Route path='/order' element={<PlaceOrder />} />
         </Routes>
       </main>
       <Footer />
